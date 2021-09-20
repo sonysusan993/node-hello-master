910cbc282b2e4c826e96eeed1b4c7b8e190cbcd3
SonySusanEapen <sony@aimabms.com>
2021-09-20 14:10:28 +0530

    npm commands

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -3,4 +3,7 @@
 mkdir /home/ec2-user/HelloNodeApp
 cd /home/ec2-user/HelloNodeApp
 
-sudo npm install
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
+. ~/.nvm/nvm.sh
+nvm install node
+npm install ng-common
--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
 # Install node.js
-npm install
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
+. ~/.nvm/nvm.sh
+nvm install node
+npm install ng-common
 
