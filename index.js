c9879ba7b924b827ad653cd50f4887984d274911
SonySusanEapen <sony@aimabms.com>
2021-09-20 17:40:38 +0530

    build 2

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const port = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  const msg = 'Hello Sony Susan Eapen! Build one\n'
+  const msg = 'Hello Sony Susan Eapen! Build Two\n'
   res.end(msg);
 });
 
