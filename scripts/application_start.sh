308587ff7e51b930b4c81cf3cf37ea3251f529be
SonySusanEapen <sony@aimabms.com>
2021-09-20 14:40:16 +0530

    1

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
 # Stop all servers and start the server as a daemon
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
+. ~/.nvm/nvm.sh
+nvm install node -g
+
 npm install forever -g
 forever stopall
 forever start /home/ec2-user/HelloNodeApp/index.js
